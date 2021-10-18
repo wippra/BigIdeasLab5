401a27bd1a826034ee0abf48b1c558f05aa8ebec
Dylan Jenny <drj29@pitt.edu>
2021-10-18 14:24:51 -0400

    I added a comparison of the given name's length and the given int's value.

--- something.py
+++ something.py
@@ -2,3 +2,13 @@ name = input("What's your name? ")
 print(f"Hi {name}!")
 given = input("Now give me an Integer!")
 given = int(given)
+
+nameLength = int(len(name))
+if (nameLength<given):
+    print("Your name has "+str(nameLength)+" characters, which is less than your given integer's value.")
+elif (nameLength>given):
+    print("Your name has "+str(nameLength)+" characters, which is more than your given integer's value.")
+elif (nameLength==given):
+    print("Your name has "+str(nameLength)+" characters, which is the same as your given integer's value.")
+else:
+    print("Oops, this isn't supposed to happen!")
